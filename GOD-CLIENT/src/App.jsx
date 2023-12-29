f727684fecb9461096eb76ec0708d8efce3c8775
nde booy <ndesimoniche@gmail.com>
2023-12-29 21:13:51 +0400

    added sign- in sign-out and profile page

--- GOD-CLIENT/src/App.jsx
+++ GOD-CLIENT/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-out" element={<SignOut />} />
         </Routes>
       </BrowserRouter>
     </div>
--- GOD-CLIENT/src/pages/SignOut.jsx
+++ GOD-CLIENT/src/pages/SignOut.jsx
@@ -1,5 +1,5 @@
 const SignOut = () => {
-  return <div>sign out</div>;
+  return <div>sign out page mother fucker</div>;
 };
 
 export default SignOut;
