37b4d0f021b6ba25e904aa5e0497c46a2f108db3
nde booy <ndesimoniche@gmail.com>
2024-01-02 14:48:18 +0400

    fixed password hasing

--- BACKEND/controlers/Auth.Controler.js
+++ BACKEND/controlers/Auth.Controler.js
@@ -1,12 +1,15 @@
 const postModel = require("../model/user_model.js");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const postRequest = async (req, res) => {
   const { username, email, password } = req.body;
 
-  //password hashing with bcrypt not implimented so, we would look at the code again
-  // const hashPassword = bcrypt.hashSync(password, 10);
-  const saveToDataBase = new postModel({ username, email, password });
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const saveToDataBase = new postModel({
+    username,
+    email,
+    password: hashPassword,
+  });
   console.log(saveToDataBase);
   try {
     await saveToDataBase.save();
